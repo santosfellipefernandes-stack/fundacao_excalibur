490cbfddf454b8f3d57695d0d99429623ee88c7b
santosfellipefernandes-stack <santos.fellipefernandes@gmail.com>
2026-06-07 22:23:22 -0300

    Update oraculo.js

--- oraculo.js
+++ oraculo.js
@@ -1,25 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-  const agentData = JSON.parse(
-    localStorage.getItem("currentAgent")
-  );
-
-  const agentCode = localStorage.getItem(
-    "currentAgentCode"
-  );
+  const agentData = JSON.parse(localStorage.getItem("currentAgent"));
+  const agentCode = localStorage.getItem("currentAgentCode");
 
   if (!agentData) {
     window.location.href = "login.html";
     return;
   }
 
-  document.getElementById("agentName").textContent =
-    agentData.name;
-
-  document.getElementById("agentCode").textContent =
-    "CÓDIGO: " + agentCode;
-
-  document.getElementById("agentLevel").textContent =
-    "NÍVEL: " + agentData.level;
-
+  document.getElementById("agentName").textContent = agentData.name;
+  document.getElementById("agentCode").textContent = "CÓDIGO: " + agentCode;
+  document.getElementById("agentLevel").textContent = "NÍVEL: " + agentData.level;
 });
